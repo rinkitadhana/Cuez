f16034fa9bc30d6a502f0f5ec138c99febd048a3
Rinkit Adhana <therinkit@gmail.com>
2025-02-17 23:28:53 +0530

    database connected

---
+++ backend/src/config/database.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+  try {
+    const URI = process.env.MONGO_URI
+    if (!URI) {
+      throw new Error("MONGO_URI environment variable is not defined")
+    }
+    await mongoose.connect(URI)
+    console.log("MongoDB Connected!")
+  } catch (error) {
+    console.log("MongoDB Error: ", error)
+    process.exit(1)
+  }
+}
+export default connectDB
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
-const app = express()
+import connectDB from "./config/database"
 dotenv.config()
+
+//define
+const app = express()
 const PORT = process.env.PORT || 4000
 
+//database
+connectDB()
+
+//baseRoute
 app.get("/", (req, res) => {
   res.send("Hello China!")
 })
+
+//Listening
 app.listen(PORT, () => {
   console.log(`listening on port http://localhost:${PORT}`)
 })
