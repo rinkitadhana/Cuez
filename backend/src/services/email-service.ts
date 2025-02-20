8d06b24b7b52150abf2015f7f4361f08f8b8d648
Rinkit Adhana <therinkit@gmail.com>
2025-02-20 23:21:40 +0530

    git ignore added and email service added

---
+++ backend/src/services/email-service.ts
@@ -0,0 +1,17 @@
+import { getOTPEmailTemplate, transporter } from "config/mail"
+
+export const sendOTPEmail = async (email: string, username: string, otp: string): Promise<boolean> =>{
+    try{
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Email Verification OTP',
+            html: getOTPEmailTemplate(username, otp)
+        }
+        await transporter.sendMail(mailOptions)
+        return true
+    } catch( error){
+        console.log('Error sending email:', error)
+        return true
+    }
+}
