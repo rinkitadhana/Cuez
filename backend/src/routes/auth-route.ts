c71410a8c777ee2630e54727c16d10564be26818
Rinkit Adhana <therinkit@gmail.com>
2025-02-22 04:38:31 +0530

    route added

--- backend/src/routes/auth-route.ts
+++ backend/src/routes/auth-route.ts
@@ -2,10 +2,12 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  sendOTP,
 } from "../controllers/auth-controller"
 import express from "express"
 const router = express.Router()
 
+router.post("/send-otp", sendOTP)
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
