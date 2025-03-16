8b6be563422235f1cb7093a1687bef0cd5b10352
Rinkit Adhana <therinkit@gmail.com>
2025-03-16 13:07:40 +0530

    message block enhanced

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -1,6 +1,8 @@
 "use client"
 import React, { useEffect } from "react"
 import useMessageStore from "../store/messageStore"
+import { CircleAlert, CircleCheck } from "lucide-react"
+
 
 const Message: React.FC = () => {
   const { message, type, clearMessage } = useMessageStore()
@@ -18,12 +20,13 @@ const Message: React.FC = () => {
   if (!message) return null
 
   return (
-    <div className="fixed md:top-10 top-5 flex justify-center items-center w-full select-none">
+    <div className="fixed left-5 bottom-5 flex justify-start items-center w-full select-none">
       <div
-        className={`md:w-1/3 w-[90%] mx-4 md:mx-auto px-5 py-2.5 text-white text-center font-medium rounded-[10px] z-[1000] ${
-          type === "success" ? "bg-green-500" : "bg-red-500 "
-        }`}
+        className="w-fit px-4 py-2.5 text-white text-center font-medium rounded-full z-[1000] flex items-center gap-2 bg-zinc-800 "
       >
+        {
+        type === "success" ? <span className="text-green-500"><CircleCheck /></span> : <span className="text-red-500"><CircleAlert /></span>
+        }
         {message}
       </div>
     </div>
