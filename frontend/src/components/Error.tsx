0a24a2ec34076ffec5b566e0891f8ad906b0843a
Rinkit Adhana <therinkit@gmail.com>
2025-03-23 05:45:50 +0530

    error component removed

--- frontend/src/components/Error.tsx
+++
@@ -1,9 +0,0 @@
-const Error = ({ text }: { text: string }) => {
-  return (
-    <div className=" flex justify-center items-center w-full py-2 text-white bg-red-500/80 font-medium rounded-[10px]">
-      {text}
-    </div>
-  )
-}
-
-export default Error
