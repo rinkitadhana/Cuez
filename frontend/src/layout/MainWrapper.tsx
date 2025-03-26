0b987b9639a59a3eede64c92b965d933b3d68fa4
Rinkit Adhana <therinkit@gmail.com>
2025-03-26 14:32:28 +0530

    scroll bug fixed

--- frontend/src/layout/MainWrapper.tsx
+++ frontend/src/layout/MainWrapper.tsx
@@ -3,8 +3,8 @@ import RightSidebar from "./sidebars/RightSidebar"
 
 const MainWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex w-[1200px] mx-auto h-screen gap-4 ">
-      <div className="w-[17%] ">
+    <main className="flex w-[1200px] mx-auto h-screen gap-4 overflow-hidden">
+      <div className="w-[17%] h-full">
         <LeftSidebar />
       </div>
       <div className="w-[53%] border-x border-zinc-700 overflow-y-auto scrollbar-hide">
