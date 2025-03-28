de3cbb0d5c6fed920f324df22644c67d33336bf9
Rinkit Adhana <therinkit@gmail.com>
2025-03-29 00:23:19 +0530

    bookmark added

---
+++ frontend/src/app/bookmarks/bookmarkComponent/Header.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Header = () => {
+  return (
+    <div className="flex w-full select-none justify-start items-center border-b py-4 border-zinc-700 sticky top-0 bg-bgClr px-6 z-10">
+      <h1 className="font-semibold opacity-95">Bookmarks</h1>    
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/app/bookmarks/page.tsx
+++ frontend/src/app/bookmarks/page.tsx
@@ -1,10 +1,19 @@
-import ComingSoon from "@/components/ComingSoon"
 import MainWrapper from "@/layout/MainWrapper"
+import Header from "./bookmarkComponent/Header"
+import PostStructure from "@/components/pageComponents/PostStructure"
 
 const page = () => {
   return (
     <MainWrapper>
-      <ComingSoon />
+      <Header />
+      <div className="flex flex-col h-screen" >
+        <PostStructure />
+        <PostStructure />
+        <PostStructure />
+        <PostStructure />
+        <PostStructure />
+        <PostStructure />
+      </div>
     </MainWrapper>
   )
 }
