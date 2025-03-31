91b4518bd02e72004cb24a42783f35bcad1a8674
Rinkit Adhana <therinkit@gmail.com>
2025-03-31 19:04:55 +0530

    updated

--- frontend/src/layout/sidebars/LeftSidebar.tsx
+++ frontend/src/layout/sidebars/LeftSidebar.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import CreatePost from "@/components/pageComponents/CreatePost"
-import { SquarePen } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
