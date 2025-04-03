2dcf5988e2c3b22edf43146cfd40fb93961daa30
Rinkit Adhana <therinkit@gmail.com>
2025-04-03 23:33:30 +0530

    edit profile page added

--- frontend/src/app/profile/profileComponent/Header.tsx
+++ frontend/src/app/profile/profileComponent/Header.tsx
@@ -1,13 +1,17 @@
 import { PencilLine } from "lucide-react"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <div className="flex sticky top-0 z-50 bg-bgClr items-center justify-between border-b border-zinc-700 px-4 py-2.5">
       <h1 className="font-semibold">Profile</h1>
-      <button className="flex items-center gap-1 rounded-xl bg-zinc-800 hover:bg-zinc-700/70 transition-colors duration-200 px-3 py-1.5 border font-semibold text-sm border-zinc-700">
+      <Link
+        href="/settings/profile"
+        className="flex items-center gap-1 rounded-xl bg-zinc-800 hover:bg-zinc-700/70 transition-colors duration-200 px-3 py-1.5 border font-semibold text-sm border-zinc-700"
+      >
         <PencilLine size={16} />
         Edit Profile
-      </button>
+      </Link>
     </div>
   )
 }
