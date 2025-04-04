8264ca4945ff371488b2797124950562544cfd5c
Rinkit Adhana <therinkit@gmail.com>
2025-04-05 03:25:16 +0530

    enhanced

--- frontend/src/app/(auth)/forgot-password/page.tsx
+++ frontend/src/app/(auth)/forgot-password/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 import Input from "@/components/Input"
-import { useResetPassword } from "@/hooks/useResetPassword"
-import { useSendOtp } from "@/hooks/useSendOTP"
-import { useVerifyOtp } from "@/hooks/useVerifyOTP"
+import { useResetPassword, useSendOtp, useVerifyOtp } from "@/hooks/useAuth"
 import Footer from "@/layout/Footer"
 import useMessageStore from "@/store/messageStore"
 import {
@@ -44,13 +42,16 @@ const ForgotPassword = () => {
         setEmailError(false)
       }, 5000)
     } else {
-      setEmailError(false)
       setIsButtonDisabled(true)
+      setEmailError(false)
       setTimer(30)
+      sendOtp(email, {
+        onError: () => {
+          setIsButtonDisabled(false)
+        },
+      })
     }
-    sendOtp(email)
   }
-
 
   const handleVerifyOtp = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -66,12 +67,15 @@ const ForgotPassword = () => {
         setOtpError(false)
       }, 5000)
     }
-    verifyOtp({ email, otp }, {
-      onSuccess: (data) => {
-        setToken(data.token)
-        setOtpVerified(true)
+    verifyOtp(
+      { email, otp },
+      {
+        onSuccess: (data) => {
+          setToken(data.token)
+          setOtpVerified(true)
+        },
       }
-    })
+    )
   }
 
   const handleResetPassword = (e: React.FormEvent<HTMLFormElement>) => {
@@ -93,8 +97,7 @@ const ForgotPassword = () => {
       return
     }
     resetPassword({ password, token })
-
-  } 
+  }
 
   const formatTimer = (time: number): string => {
     return time < 10 ? `0${time}` : `${time}`
@@ -116,7 +119,6 @@ const ForgotPassword = () => {
     return () => clearInterval(interval)
   }, [timer])
 
-
   useEffect(() => {
     if (password) {
       if (confirmPassword) {
@@ -134,134 +136,131 @@ const ForgotPassword = () => {
       <div className="flex-grow sin-screen ">
         {otpVerified ? (
           <form className=" flex flex-col gap-6" onSubmit={handleResetPassword}>
-          <div className=" flex flex-col gap-4">
-            <Image
-              className=" size-50 select-none"
-              src="/img/icon/cuez-name.png"
-              height={180}
-              width={180}
-              alt="company_logo"
-            />
-            {/* <h1 className=" text-mainclr text-5xl font-bold">Cuez</h1> */}
-            <p>
-            Please enter a new password and confirm it to continue.
-            </p>
-          </div>
-          <div className=" flex flex-col gap-4">
-            <Input
-              text="Password"
-              type="password"
-              value={password}
-              error={passwordError}
-              onChange={(e) => setPassword(e.target.value)}
-              ficon={<LockKeyhole strokeWidth={1.5} />}
-              licon1={<Eye strokeWidth={1.5} />}
-              licon2={<EyeOff strokeWidth={1.5} />}
-            />
-            <Input
-              text="Confirm password"
-              type="password"
-              value={confirmPassword}
-              error={!passwordsMatch || passwordError}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              ficon={<LockKeyhole strokeWidth={1.5} />}
-              licon1={<Eye strokeWidth={1.5} />}
-              licon2={<EyeOff strokeWidth={1.5} />}
-            />
-          </div>
-          <div className=" flex flex-col gap-4">
-            <button disabled={isLoggingIn} className="blue-btn" type="submit">
-              {isLoggingIn ? (
-                <div className="flex items-center justify-center gap-2">
-                  <LoaderCircle className="animate-spin" />
-                </div>
-              ) : (
-                "Reset password"
-              )}
-            </button>{" "}
-            <div className="  flex justify-end">
-              <Link
-                href="/login"
-                className=" text-mainclr hover:text-mainclr/80 transition duration-200 cursor-pointer"
-              >
-                Back to Log in
-              </Link>
+            <div className=" flex flex-col gap-4">
+              <Image
+                className=" size-50 select-none"
+                src="/img/icon/cuez-name.png"
+                height={180}
+                width={180}
+                alt="company_logo"
+              />
+              {/* <h1 className=" text-mainclr text-5xl font-bold">Cuez</h1> */}
+              <p>Please enter a new password and confirm it to continue.</p>
             </div>
+            <div className=" flex flex-col gap-4">
+              <Input
+                text="Password"
+                type="password"
+                value={password}
+                error={passwordError}
+                onChange={(e) => setPassword(e.target.value)}
+                ficon={<LockKeyhole strokeWidth={1.5} />}
+                licon1={<Eye strokeWidth={1.5} />}
+                licon2={<EyeOff strokeWidth={1.5} />}
+              />
+              <Input
+                text="Confirm password"
+                type="password"
+                value={confirmPassword}
+                error={!passwordsMatch || passwordError}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                ficon={<LockKeyhole strokeWidth={1.5} />}
+                licon1={<Eye strokeWidth={1.5} />}
+                licon2={<EyeOff strokeWidth={1.5} />}
+              />
+            </div>
+            <div className=" flex flex-col gap-4">
+              <button disabled={isLoggingIn} className="blue-btn" type="submit">
+                {isLoggingIn ? (
+                  <div className="flex items-center justify-center gap-2">
+                    <LoaderCircle className="animate-spin" />
+                  </div>
+                ) : (
+                  "Reset password"
+                )}
+              </button>{" "}
+              <div className="  flex justify-end">
+                <Link
+                  href="/login"
+                  className=" text-mainclr hover:text-mainclr/80 transition duration-200 cursor-pointer"
+                >
+                  Back to Log in
+                </Link>
+              </div>
             </div>
           </form>
         ) : (
           <form className=" flex flex-col gap-6" onSubmit={handleVerifyOtp}>
             <div className=" flex flex-col gap-4">
-            <Image
-              className=" size-50 select-none"
-              src="/img/icon/cuez-name.png"
-              height={180}
-              width={180}
-              alt="company_logo"
-            />
-            {/* <h1 className=" text-mainclr text-5xl font-bold">Cuez</h1> */}
-            <p>
-              Enter your registered email address and the correct OTP to
-              continue.
-            </p>
-          </div>
-          <div className=" flex flex-col gap-4">
-            <Input
-              text="Email address"
-              type="email"
-              value={email}
-              error={emailError}
-              onChange={(e) => setEmail(e.target.value)}
-              ficon={<Mail strokeWidth={1.5} />}
-            />
+              <Image
+                className=" size-50 select-none"
+                src="/img/icon/cuez-name.png"
+                height={180}
+                width={180}
+                alt="company_logo"
+              />
+              {/* <h1 className=" text-mainclr text-5xl font-bold">Cuez</h1> */}
+              <p>
+                Enter your registered email address and the correct OTP to
+                continue.
+              </p>
+            </div>
+            <div className=" flex flex-col gap-4">
+              <Input
+                text="Email address"
+                type="email"
+                value={email}
+                error={emailError}
+                onChange={(e) => setEmail(e.target.value)}
+                ficon={<Mail strokeWidth={1.5} />}
+              />
 
-            <div className="flex items-center gap-3 w-full">
-              <div className="flex-1">
-                <Input
-                  text="Code"
-                  type="text"
-                  value={otp}
-                  error={otpError}
-                  onChange={(e) => setOtp(e.target.value)}
-                  ficon={<Hash strokeWidth={1.5} />}
-                />
-              </div>
-              <button
-                className={` ${
-                  isButtonDisabled ? "trans-btn-n " : "trans-btn"
-                }`}
-                onClick={handleSendOTP}
-                disabled={isButtonDisabled}
-                type="button"
-              >
-                {isButtonDisabled
-                  ? `Resend in ${formatTimer(timer)}s`
-                  : "Send code"}
-              </button>
-            </div>
-          </div>
-          <div className=" flex flex-col gap-4">
-            <button className="blue-btn" type="submit">
-              {isLoggingIn ? (
-                <div className="flex items-center justify-center gap-2">
-                  <LoaderCircle className="animate-spin" />
+              <div className="flex items-center gap-3 w-full">
+                <div className="flex-1">
+                  <Input
+                    text="Code"
+                    type="text"
+                    value={otp}
+                    error={otpError}
+                    onChange={(e) => setOtp(e.target.value)}
+                    ficon={<Hash strokeWidth={1.5} />}
+                  />
                 </div>
-              ) : (
-                "Continue"
-              )}
-            </button>{" "}
-            <div className="  flex justify-end">
-              <Link
-                href="/login"
-                className=" text-mainclr hover:text-mainclr/80 transition duration-200 cursor-pointer"
-              >
-                Back to Log in
-              </Link>
+                <button
+                  className={` ${
+                    isButtonDisabled ? "trans-btn-n " : "trans-btn"
+                  }`}
+                  onClick={handleSendOTP}
+                  disabled={isButtonDisabled}
+                  type="button"
+                >
+                  {isButtonDisabled
+                    ? `Resend in ${formatTimer(timer)}s`
+                    : "Send code"}
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
-          )
-        }
+            <div className=" flex flex-col gap-4">
+              <button className="blue-btn" type="submit">
+                {isLoggingIn ? (
+                  <div className="flex items-center justify-center gap-2">
+                    <LoaderCircle className="animate-spin" />
+                  </div>
+                ) : (
+                  "Continue"
+                )}
+              </button>{" "}
+              <div className="  flex justify-end">
+                <Link
+                  href="/login"
+                  className=" text-mainclr hover:text-mainclr/80 transition duration-200 cursor-pointer"
+                >
+                  Back to Log in
+                </Link>
+              </div>
+            </div>
+          </form>
+        )}
       </div>
       <Footer />
     </section>
