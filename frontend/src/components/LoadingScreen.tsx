ed3a6cc1ff87adbd2ac824095eb8c26feb81daa5
Rinkit Adhana <therinkit@gmail.com>
2025-04-06 03:33:57 +0530

    loading enhanced

--- frontend/src/components/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen.tsx
@@ -1,21 +1,6 @@
-"use client"
-
-import { useEffect, useState } from "react"
 import Image from "next/image"
 
 export default function LoadingScreen() {
-  const [show, setShow] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShow(false)
-    }, 1000)
-
-    return () => clearTimeout(timer)
-  }, [])
-
-  if (!show) return null
-
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-bgClr select-none">
       <div className="flex flex-col items-center">
