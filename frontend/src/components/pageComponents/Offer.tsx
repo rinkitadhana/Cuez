0560066548fd51d3d5371d3f3a28b4695f497abb
Rinkit Adhana <therinkit@gmail.com>
2025-04-06 04:08:48 +0530

    added offer

---
+++ frontend/src/components/pageComponents/Offer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Offer = () => {
+  return (
+    <div className=" text-sm text-center text-zinc-200 border p-2 rounded-xl border-zinc-700 bg-zinc-800/50">
+      ✨ Give us your feedback and get a cuez batch.
+      </div>
+  )
+}
+
+export default Offer
--- frontend/src/layout/sidebars/RightSidebar.tsx
+++ frontend/src/layout/sidebars/RightSidebar.tsx
@@ -5,6 +5,7 @@ import SuggestedUsers from "@/components/pageComponents/SuggestedUsers"
 import FeedbackForm from "@/components/FeedbackForm"
 import BetaNotification from "@/components/pageComponents/BetaNotification"
 import GithubStar from "@/components/pageComponents/GithubStar"
+import Offer from "@/components/pageComponents/Offer"
 const RightSidebar = () => {
   const pathname = usePathname()
   return (
@@ -12,6 +13,7 @@ const RightSidebar = () => {
       {!pathname.includes("/search") && <SearchBox />}
       <BetaNotification />
       <SuggestedUsers />
+      <Offer/>
       <FeedbackForm />
       <GithubStar />
     </section>
