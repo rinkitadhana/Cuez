8bd87360ab6f76420412d72707cfbb58e7ea4752
Rinkit Adhana <therinkit@gmail.com>
2025-04-09 23:46:22 +0530

    fixed for larger uploads

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,8 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 //middlewares
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(cookieParser())
 app.use(
   cors({
