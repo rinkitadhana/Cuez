7d119f2d264d01296d41cad741fc209517e85927
Rinkit Adhana <therinkit@gmail.com>
2025-04-14 01:19:21 +0530

    config updated and fixed

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,7 @@
 const config = {
   backendUrl:
     process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080/api",
+  frontendUrl: process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000",
 }
 
 export default config
