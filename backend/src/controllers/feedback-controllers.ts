39fd6b50b8d2f4a53f0f1db9ad1a1255e33310bd
Rinkit Adhana <therinkit@gmail.com>
2025-04-22 16:47:46 +0530

    feedback logic added

---
+++ backend/src/controllers/feedback-controllers.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express"
+import { Feedback } from "../models/feedback-model"
+import { errorHandler } from "../utils/errorHandler"
+import User from "../models/user-model"
+
+const createFeedback = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { title, description } = req.body
+    const user = await User.findById(req.user._id)
+
+    if (!user) {
+      res.status(400).json({ message: "UnAuthorized user!" })
+      return
+    }
+    if (!title || !description) {
+      res.status(400).json({ message: "All fields are required!" })
+      return
+    }
+    const feedback = new Feedback({
+      title,
+      description,
+      user: req.user._id,
+    })
+    await feedback.save()
+    if (!user.cuezBadge) {
+      user.cuezBadge = true
+      await user.save()
+    }
+    res.status(201).json({ message: "Feedback created successfully" })
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
+
+const getFeedbacks = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const feedbacks = await Feedback.find()
+      .populate({
+        path: "User",
+        select: "-password",
+      })
+      .sort({ createdAt: -1 })
+    res
+      .status(200)
+      .json({ feedbacks, message: "All Feebacks are fetched sucessfully!" })
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
+
+export { createFeedback, getFeedbacks }
