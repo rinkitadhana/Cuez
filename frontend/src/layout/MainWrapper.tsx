6d250b8ba37d479ab21d7d120b5cdcc16c897057
Rinkit Adhana <therinkit@gmail.com>
2025-04-29 20:02:23 +0530

    fix

--- frontend/src/layout/MainWrapper.tsx
+++ frontend/src/layout/MainWrapper.tsx
@@ -7,11 +7,11 @@ import PostButton from "@/components/pageComponents/PostButton"
 const MainWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <main className="flex justify-center lg:w-[1200px] w-full mx-auto h-screen gap-4 overflow-hidden pb-10 md:pb-0">
+      <main className="flex justify-center lg:w-[1200px] w-full mx-auto h-screen gap-4 overflow-hidden ">
         <div className="lg:w-[17%] md:w-[25%] w-full h-full hidden md:block md:pl-4">
           <LeftSidebar />
         </div>
-        <div className="relative lg:w-[53%] md:w-full w-full md:border-x md:border-zinc-700 overflow-y-auto scrollbar-hide">
+        <div className="relative lg:w-[53%] md:w-full w-full md:border-x md:border-zinc-700 overflow-y-auto scrollbar-hide mb-10 md:mb-0">
           <PostButton />
           {children}
         </div>
