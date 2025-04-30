ebaa329cf87275e5aac19cc5c7f5c2b95b5eb269
Rinkit Adhana <therinkit@gmail.com>
2025-05-01 01:27:17 +0530

    route updated

--- backend/src/routes/post-route.ts
+++ backend/src/routes/post-route.ts
@@ -1,7 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middlewares/ProtectRoute"
 import {
-  commentPost,
   createPost,
   deletePost,
   getAllPosts,
@@ -16,6 +15,8 @@ import {
   bookmarkPost,
   getBookmarkedPosts,
   isBookmarked,
+  getThread,
+  getReplies,
 } from "../controllers/post-controller"
 const router = express.Router()
 
@@ -24,7 +25,6 @@ router.patch("/edit-post/:id", protectRoute, editPost)
 router.delete("/delete-post/:id", protectRoute, deletePost)
 router.post("/like-post/:id", protectRoute, likeUnlikePost)
 router.get("/is-liked/:id", protectRoute, isLiked)
-router.post("/comment-post/:id", protectRoute, commentPost)
 router.get("/all-posts", getAllPosts)
 router.get("/following-posts", protectRoute, getFollowingPosts)
 router.get("/trending-posts", getTrendingPosts)
@@ -33,6 +33,8 @@ router.get("/user-posts/:username", protectRoute, getUserPosts)
 router.post("/bookmark-post/:id", protectRoute, bookmarkPost)
 router.get("/is-bookmarked/:id", protectRoute, isBookmarked)
 router.get("/bookmarked-posts", protectRoute, getBookmarkedPosts)
+router.get("/thread/:id", getThread)
+router.get("/replies/:id", getReplies)
 
 router.get("/post/:id", getPostById)
 export default router
