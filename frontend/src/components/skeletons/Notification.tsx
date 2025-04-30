c10769a71123da9bd0d0bf0a5fb805b26908ba43
Rinkit Adhana <therinkit@gmail.com>
2025-05-01 02:39:15 +0530

    notification skeleton fixed

--- frontend/src/components/skeletons/Notification.tsx
+++ frontend/src/components/skeletons/Notification.tsx
@@ -1,9 +1,8 @@
 const Notification = () => {
   return (
     <div className="flex px-4 sm:px-6 py-4 border-b border-zinc-800 animate-pulse">
-      <div className="relative shrink-0 mr-3 sm:mr-4">
+      <div className=" shrink-0 mr-3 sm:mr-4">
         <div className="w-10 h-10 sm:w-12 sm:h-12 rounded-full sm:rounded-xl bg-zinc-800" />
-        <div className="absolute -right-1 -bottom-1 w-4 h-4 rounded-full bg-zinc-700" />
       </div>
 
       <div className="flex flex-col flex-1 min-w-0">
@@ -11,11 +10,10 @@ const Notification = () => {
           <div className="h-4 w-24 sm:w-32 bg-zinc-800 rounded-full" />
           <div className="h-4 w-20 sm:w-24 bg-zinc-800 rounded-full" />
         </div>
-        <div className="h-4 w-16 bg-zinc-800 rounded-full mt-2" />
-        <div className="h-20 w-full max-w-[90%] bg-zinc-800/50 rounded-lg mt-3" />
+        <div className="h-4 w-[80%] bg-zinc-800 rounded-full mt-2" />
       </div>
 
-      <div className="w-8 h-8 bg-zinc-800/50 rounded-full self-start" />
+      <div className="w-8 h-8 bg-zinc-800/50 rounded-xl self-start" />
     </div>
   )
 }
