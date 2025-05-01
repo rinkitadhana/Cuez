411896bbd1124ec745d3f5f02de633c959bcd8c9
Rinkit Adhana <therinkit@gmail.com>
2025-05-01 21:04:23 +0530

    beta noti removed

--- frontend/src/layout/sidebars/RightSidebar.tsx
+++ frontend/src/layout/sidebars/RightSidebar.tsx
@@ -3,7 +3,7 @@ import { SearchBox } from "@/components/pageComponents/SearchBox"
 import { usePathname } from "next/navigation"
 import SuggestedUsers from "@/components/pageComponents/SuggestedUsers"
 import FeedbackForm from "@/components/FeedbackForm"
-import BetaNotification from "@/components/pageComponents/BetaNotification"
+// import BetaNotification from "@/components/pageComponents/BetaNotification"
 import GithubStar from "@/components/pageComponents/GithubStar"
 import Offer from "@/components/pageComponents/Offer"
 const RightSidebar = () => {
@@ -11,9 +11,9 @@ const RightSidebar = () => {
   return (
     <section className="flex flex-col gap-4 my-4 w-full">
       {!pathname.includes("/search") && <SearchBox />}
-      <BetaNotification />
+      {/* <BetaNotification /> */}
       <SuggestedUsers />
-      <Offer/>
+      <Offer />
       <FeedbackForm />
       <GithubStar />
     </section>
