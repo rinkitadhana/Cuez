d72b9eac7577672bbb5d513d2b172c9a486bcb97
Rinkit Adhana <therinkit@gmail.com>
2026-05-29 04:44:48 +0530

    Allow Cloudinary URLs via Next image remotePatterns

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   images: {
     domains: ["www.shutterstock.com", "flowbite.com", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
 }
 
